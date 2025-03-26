eafc854692c302ffb8a985a3684969d32a1cb9a6
Patryk Siuda <patryksiudaprogrammer@gmail.com>
2025-03-26 14:27:02 +0100

    Added vulkan instance creation

--- src/VulkanApplication.cpp
+++ src/VulkanApplication.cpp
@@ -1,69 +1,98 @@
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
 
 class VulkanApplication {
 public:
-    void run() {
-        initWindow();
-        initVulkan();
-        gameLoop();
-        cleanup();
-    }
+  void run() {
+    initWindow();
+    initVulkan();
+    gameLoop();
+    cleanup();
+  }
 
 private:
-    GLFWwindow* window;
+  GLFWwindow *window;
+  const uint32_t WIDTH = 1280;
+  const uint32_t HEIGHT = 720;
 
-    void initWindow() {
-        glfwInit();
+  VkInstance instance;
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        window = glfwCreateWindow(1280, 720, "Hello Vulkan", nullptr, nullptr);
+  void initWindow() {
+    glfwInit();
 
-        uint32_t extensionCount = 0;
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Hello Vulkan", nullptr, nullptr);
 
-        // todo: do not use cout, replace with print or other modern c++ solution
-        std::cout << extensionCount << " extensions supported\n";
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    // todo: do not use cout, replace with print or other modern c++ solution
+    std::cout << extensionCount << " extensions supported\n";
+  }
+
+  void initVulkan() {
+    testGlm();
+    createInstance();
+  }
+
+  void testGlm() {
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+  }
+
+  void createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.pNext = nullptr;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
     }
+  }
 
-    void initVulkan() {
-        // testing glm library
-        glm::mat4 matrix;
-        glm::vec4 vec;
-        auto test = matrix * vec;
+  void gameLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
     }
+  }
 
-    void gameLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
-
-    void cleanup() {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
+  void cleanup() {
+    vkDestroyInstance(instance, nullptr);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
 };
 
 int main() {
-    VulkanApplication app;
+  VulkanApplication app;
 
-    try {
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
